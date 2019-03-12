e4ffec958f4ccd29959db14d75c54deed9baab52
Admin <geovisionsalprojects@gmail.com>
2019-03-12 14:11:49 +0200

    updates

--- quicktools/src/main/java/com/rzahr/quicktools/QuickAppModule.kt
+++ quicktools/src/main/java/com/rzahr/quicktools/QuickAppModule.kt
@@ -1,19 +1,16 @@
 package com.rzahr.quicktools
 
-import android.annotation.SuppressLint
 import android.app.Application
-import android.app.Notification
-import android.app.NotificationManager
 import android.content.Context
 import dagger.Module
 import dagger.Provides
-import javax.inject.Named
 import javax.inject.Scope
 
 @Scope
 @Retention(value = AnnotationRetention.RUNTIME)
 annotation class ApplicationScope
 
+@Suppress("unused", "MemberVisibilityCanBePrivate")
 @Module
 class QuickAppModule constructor(val context: Context, @Suppress("MemberVisibilityCanBePrivate") val rapidIdler: QuickClickGuard, val application: Application) {
 
